cd496892f35f0a0a86bbef631868e8d05ba37b8c
justin moore <justin.moore.jmo@gmail.com>
2019-04-23 13:44:10 -0500

    Initial Commit

---
+++ Art Clicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Art Clicker
+//
+//  Created by Justin Moore on 4/23/19.
+//  Copyright © 2019 Justin Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
